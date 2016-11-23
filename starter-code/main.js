32aa01ccfafbd58f031eac59d55ed53b4e823c55
System Administrator <root@RYVYL.local>
2016-11-22 19:16:50 -0800

    Created Game Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,5 +3,26 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+if (cardOne === cardThree)
+	else if (cardOne === cardTwo)
+	else if ()
+	alert('You Found a Match!')
 
+alert('You Found a Match!')
 console.log("JS file is connected to HTML! Woo!")
+
+
+if (cardOne === cardThree) {
+	alert("You have a Match!")
+}
+else if (cardOne === cardTwo, cardFour) {
+	alert("sorry")
+}
+else if (cardTwo === cardFour) {
+	alert("You have a Match!")
+}
+else if (cardTwo === cardOne, cardThree) {
+	alert("sorry")
+}
+
+}
